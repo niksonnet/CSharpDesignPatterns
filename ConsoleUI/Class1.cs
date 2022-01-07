a5e6d7e22a04a7ff4e0f3f4cbdf098bbc848379a
niksonnet <nikpatel10@live.com>
2022-01-07 16:45:31 +0530

    Add project files.

---
+++ ConsoleUI/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    public class Class1
+    {
+        public static readonly TableServers host1 = TableServers.GetTableServers();
+        public static readonly TableServers host2 = TableServers.GetTableServers();
+        public static void Main()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GetHost1servers();
+                GetHost2servers();
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void GetHost1servers()
+        {
+            Console.WriteLine("The next Sevver is {0}", host1.GetNextServer());
+        }
+        public static void GetHost2servers()
+        {
+            Console.WriteLine("The next Sevver is {0}", host1.GetNextServer());
+        }
+    }
+}
---
+++ ConsoleUI/TableServers.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    public class TableServers
+    {
+        private static readonly TableServers _instance = new TableServers();
+        public List<string> servers = new List<string>();
+        private int nextServer = 0;
+        private TableServers()
+        {
+            servers.Add("sevrer_1");
+            servers.Add("sevrer_2");
+            servers.Add("sevrer_3");
+            servers.Add("sevrer_4");
+            servers.Add("sevrer_5");
+        }
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+
+        public string GetNextServer() 
+        {
+            string output = servers[nextServer];
+            nextServer += 1;
+
+            if (nextServer >= servers.Count)
+            {
+                nextServer = 0;
+            }
+            return output;
+        }
+    }
+}
